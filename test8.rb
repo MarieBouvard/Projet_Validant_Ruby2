0a298e210b2cbd641e4592af0aaac3514cf8efad
MarieBouvard <mariebouvard@outlook.com>
2021-07-08 21:51:23 +0200

    Exercices projet validant

---
+++ test8.rb
@@ -0,0 +1,165 @@
+###### Partie 8 : Répartition des handles #########
+
+
+def ask_chaine
+	chaine = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+return chaine
+end 
+
+
+def ask_arry_1
+	arry_1=Array.new
+return arry_1
+end
+
+def ask_arry_2
+	arry_2=Array.new
+return arry_2
+end
+
+def ask_arry_3
+	arry_3=Array.new
+return arry_3
+end
+
+def ask_arry_4
+	arry_4=Array.new
+return arry_4
+end
+
+def ask_arry_5
+	arry_5=Array.new
+return arry_5
+end
+
+def ask_arry_1
+	arry_1=Array.new
+return arry_1
+end
+
+def ask_arry_6
+	arry_6=Array.new
+return arry_6
+end
+
+def ask_arry_7
+	arry_7=Array.new
+return arry_7
+end
+
+def ask_arry_8
+	arry_8=Array.new
+return arry_8
+end
+
+def ask_arry_9
+	arry_9=Array.new
+return arry_9
+end
+
+def ask_arry_10
+	arry_10=Array.new
+return arry_10
+end
+
+def ask_arry_11
+	arry_11=Array.new
+return arry_11
+end
+
+def ask_arry_12
+	arry_12=Array.new
+return arry_12
+end
+	
+	
+
+def repartition(chaine, arry_1, arry_2, arry_3, arry_4, arry_5, arry_6, arry_7, arry_8, arry_9, arry_10, arry_11, arry_12)
+
+chaine.each do |element|
+
+		if element.length == 1 then
+			arry_1.push(element)
+		
+		elsif element.length == 2 then
+			arry_2.push(element)
+		
+		elsif element.length == 3 then
+			arry_3.push(element)
+			
+		elsif element.length == 4 then
+			arry_4.push(element)
+			
+		elsif element.length == 5 then
+			arry_5.push(element)
+			
+		elsif element.length == 6 then
+			arry_6.push(element)	
+			
+		elsif element.length == 7 then
+			arry_7.push(element)	
+		
+		elsif element.length == 8 then
+			arry_8.push(element)
+			
+		elsif element.length == 9 then
+			arry_9.push(element)
+			
+		elsif element.length == 10 then
+			arry_10.push(element)	
+			
+		elsif element.length == 11 then
+			arry_11.push(element)	
+		
+		elsif element.length == 12 then
+			arry_12.push(element)
+		end
+		end
+			puts "Le array avec 1 élement sont #{arry_1}"
+			puts ""
+			puts "Le array avec 2 élements sont #{arry_2}"
+			puts ""
+			puts "Le array avec 3 élements sont #{arry_3}"
+			puts ""
+			puts "Le array avec 4 élements sont #{arry_4}"
+			puts ""
+			puts "Le array avec 5 élements sont #{arry_5}"
+			puts ""
+			puts "Le array avec 6 élements sont #{arry_6}"
+			puts ""
+			puts "Le array avec 7 élements sont #{arry_7}"
+			puts ""
+			puts "Le array avec 8 élements sont #{arry_8}"
+			puts ""
+			puts "Le array avec 9 élements sont #{arry_9}"
+			puts ""
+			puts "Le array avec 10 élements sont #{arry_10}"
+			puts ""
+			puts "Le array avec 11 élements sont #{arry_11}"
+			puts ""
+			puts "Le array avec 12 élements sont #{arry_12}"
+			puts ""
+		
+		
+end
+
+
+def perform 
+	chaine = ask_chaine
+	arry_1 = ask_arry_1
+	arry_2 = ask_arry_2
+	arry_3 = ask_arry_3
+	arry_4 = ask_arry_4
+	arry_5 = ask_arry_5
+	arry_6 = ask_arry_6
+	arry_7 = ask_arry_7
+	arry_8 = ask_arry_8
+	arry_9 = ask_arry_9
+	arry_10 = ask_arry_10
+	arry_11 = ask_arry_11
+	arry_12 = ask_arry_12
+	repartition(chaine, arry_1, arry_2, arry_3, arry_4, arry_5, arry_6, arry_7, arry_8, arry_9, arry_10, arry_11, arry_12)
+end
+
+perform
